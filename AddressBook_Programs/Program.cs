c64a7ab0a32a651dd634a9b3d367a65f2d628a78
Pranalijadhav03 <jadhav.pranali2403@gmail.com>
2022-05-26 12:58:56 +0530

    Adition: Create a contact program_UC1

---
+++ AddressBook_Programs/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AddressBook_Programs;
+
+Console.WriteLine("====Welcome to Address Book Program====");
+AddressBookEntry addressBook = new AddressBookEntry();
+Contact newContact = AddressBookEntry.CreateContact();
+Console.WriteLine("*****-> Details Of Person  <-*****");
+Console.WriteLine($"First Name: {newContact.FirstName}");
+Console.WriteLine($"Last Name: {newContact.LastName}");
+Console.WriteLine($"Address: {newContact.Address}");
+Console.WriteLine($"City Name: {newContact.City}");
+Console.WriteLine($"State Name: {newContact.State}");
+Console.WriteLine($"Postalcode: {newContact.PostalCode}");
+Console.WriteLine($"PhoneNumber: {newContact.PhoneNumber}");
+Console.WriteLine($"Email: {newContact.Email}");
+Console.WriteLine("==========================================================================");
+Console.ReadLine();
